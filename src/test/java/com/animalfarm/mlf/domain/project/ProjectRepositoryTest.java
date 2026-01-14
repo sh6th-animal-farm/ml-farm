cc587f02ad6a1ccde11c90a8b4011610b27e40c9
limsejun <lsj1137@naver.com>
2026-01-14 16:49:08 +0900

    [FEAT] 프로젝트 조회 & 검색 기능 테스트 코드 추가1

---
+++ src/test/java/com/animalfarm/mlf/domain/project/ProjectRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.animalfarm.mlf.domain.project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
