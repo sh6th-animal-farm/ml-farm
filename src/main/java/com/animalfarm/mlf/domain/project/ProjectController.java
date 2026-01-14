c6fd372c8811daa4cf350a88307fd37afd017816
sari <songseong88@naver.com>
2026-01-14 16:02:18 +0900

    [FEAT] 프로젝트 상세 조회 기능 추가

--- src/main/java/com/animalfarm/mlf/domain/project/ProjectController.java
+++ src/main/java/com/animalfarm/mlf/domain/project/ProjectController.java
@@ -4,18 +4,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.animalfarm.mlf.domain.project.dto.ProjectDTO;
+import com.animalfarm.mlf.domain.project.dto.ProjectDetailDTO;
 
 @RestController
 public class ProjectController {
-	
+
 	@Autowired
 	ProjectService projectService;
-	
+
 	@GetMapping("/projects")
 	public List<ProjectDTO> selectAll() {
 		return projectService.selectAll();
+	}
+
+	@GetMapping("/api/project/{projectId}")
+	public ProjectDetailDTO selectDetail(@PathVariable("projectId")
+	Long projectId) {
+		return projectService.selectDetail(projectId);
 	}
 }
--- src/main/java/com/animalfarm/mlf/domain/project/ProjectInterface.java
+++ src/main/java/com/animalfarm/mlf/domain/project/ProjectInterface.java
@@ -3,7 +3,10 @@ package com.animalfarm.mlf.domain.project;
 import java.util.List;
 
 import com.animalfarm.mlf.domain.project.dto.ProjectDTO;
+import com.animalfarm.mlf.domain.project.dto.ProjectDetailDTO;
 
 public interface ProjectInterface {
 	public abstract List<ProjectDTO> selectAll();
+
+	public abstract ProjectDetailDTO selectDetail(Long projectId);
 }
