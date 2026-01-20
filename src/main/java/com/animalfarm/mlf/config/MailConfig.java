ae88d6f4aa4262396ceb8996663eba270db0885c
yongjun <dydwns6837@naver.com>
2026-01-20 18:18:20 +0900

    [BUILD] 이메일 설정 파일 추가

--- src/main/java/com/animalfarm/mlf/config/MailConfig.java
+++ src/main/java/com/animalfarm/mlf/config/MailConfig.java
@@ -2,29 +2,46 @@ package com.animalfarm.mlf.config;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
+@PropertySource("classpath:config/mail.properties")
 public class MailConfig {
 
 	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Value("${mail.host}")
+	private String host;
+
+	@Value("${mail.port}")
+	private int port;
+
+	@Value("${mail.username}")
+	private String username;
+
+	@Value("${mail.password}")
+	private String password;
+
+	@Bean
 	public JavaMailSender javaMailSender() {
-
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
-		mailSender.setHost("smtp.gmail.com");
-		mailSender.setPort(587);
-		mailSender.setUsername("malfarm0114@gmail.com");
-		mailSender.setPassword("kttw ntyl nrza tqtn");
+		mailSender.setHost(host);
+		mailSender.setPort(port);
+		mailSender.setUsername(username);
+		mailSender.setPassword(password);
 
 		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.debug", "true");
 
 		return mailSender;
 	}
