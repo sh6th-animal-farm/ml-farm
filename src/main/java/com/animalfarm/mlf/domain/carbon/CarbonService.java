837bbded19ba6b3db41336ce006e2c19ecb0de38
yongjun <dydwns6837@naver.com>
2026-01-21 12:30:32 +0900

    [FEAT] 탄소마켓 전체 조회 로직 구현

--- src/main/java/com/animalfarm/mlf/domain/carbon/CarbonService.java
+++ src/main/java/com/animalfarm/mlf/domain/carbon/CarbonService.java
@@ -1,5 +1,23 @@
 package com.animalfarm.mlf.domain.carbon;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.animalfarm.mlf.domain.carbon.dto.CarbonListDTO;
+
+@Service
 public class CarbonService {
 
+	@Autowired
+	private CarbonRepository carbonRepository;
+
+	public List<CarbonListDTO> selectAll() {
+		return carbonRepository.selectAll();
+	}
+
+	public List<CarbonListDTO> selectByCategory(String category) {
+		return carbonRepository.selectByCategory(category);
+	}
 }
