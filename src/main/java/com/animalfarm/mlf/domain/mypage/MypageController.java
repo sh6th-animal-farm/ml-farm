309838aaf59ad1e677362be52714ca13a489b28f
yongjun <dydwns6837@naver.com>
2026-01-30 11:29:55 +0900

    [FEAT] 마이페이지 내정보 user 테이블에서 불러와 값 추가하는 로직 & jsp 구현

--- src/main/java/com/animalfarm/mlf/domain/mypage/MypageController.java
+++ src/main/java/com/animalfarm/mlf/domain/mypage/MypageController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.animalfarm.mlf.domain.mypage.dto.CarbonHistoryDTO;
+import com.animalfarm.mlf.domain.mypage.dto.PasswordUpdateRequestDTO;
+import com.animalfarm.mlf.domain.mypage.dto.ProfileDTO;
+import com.animalfarm.mlf.domain.mypage.dto.ProfileUpdateRequestDTO;
 
 @RestController
 @RequestMapping("/api/mypage")
@@ -21,5 +26,24 @@ public class MypageController {
 	public ResponseEntity<List<CarbonHistoryDTO>> getCarbonHistory() {
 		// 서비스 내부에서 유저 ID를 조회하도록 설계된 메서드를 호출합니다.
 		return ResponseEntity.ok(mypageService.getCarbonHistory());
+	}
+
+	@GetMapping("/profile")
+	public ResponseEntity<ProfileDTO> getProfile() {
+		return ResponseEntity.ok(mypageService.getProfile());
+	}
+
+	@PatchMapping("/profile")
+	public ResponseEntity<Void> updateProfile(@RequestBody
+	ProfileUpdateRequestDTO req) {
+		mypageService.updateProfile(req);
+		return ResponseEntity.ok().build();
+	}
+
+	@PatchMapping("/password")
+	public ResponseEntity<Void> updatePassword(@RequestBody
+	PasswordUpdateRequestDTO dto) {
+		mypageService.updatePassword(dto);
+		return ResponseEntity.ok().build();
 	}
 }
--- src/main/java/com/animalfarm/mlf/domain/mypage/MypageRepository.java
+++ src/main/java/com/animalfarm/mlf/domain/mypage/MypageRepository.java
@@ -3,10 +3,30 @@ package com.animalfarm.mlf.domain.mypage;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.animalfarm.mlf.domain.mypage.dto.CarbonHistoryDTO;
+import com.animalfarm.mlf.domain.mypage.dto.ProfileDTO;
+import com.animalfarm.mlf.domain.mypage.dto.ProfileUpdateRequestDTO;
 
 @Mapper
 public interface MypageRepository {
 	List<CarbonHistoryDTO> selectCarbonHistoryByUserId(Long userId);
+
+	ProfileDTO selectProfile(Long userId);
+
+	int updateProfile(@Param("userId")
+	Long userId,
+		@Param("req")
+		ProfileUpdateRequestDTO req);
+
+	// 현재 비밀번호 조회
+	String selectPasswordByUserId(Long userId);
+
+	// 비밀번호 업데이트
+	int updatePassword(
+		@Param("userId")
+		Long userId,
+		@Param("password")
+		String password);
 }
