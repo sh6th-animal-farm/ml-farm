6fa5c7cdb88efedf3199f758659142dbb4182f6e
sari <songseong88@naver.com>
2026-02-02 00:01:16 +0900

    [CHORE] 구문 오류 수정

--- src/main/webapp/resources/js/domain/project/project_api.js
+++ src/main/webapp/resources/js/domain/project/project_api.js
@@ -3,13 +3,13 @@ import { http } from "../../api/http_client.js";
 
 let commonUrl = `${ctx}/api/project`;
 export const ProjectApi = {
-  getAll: () => http.get(`${commonUrl}}/all`),
-  getPictures: (id) => http.get(`${commonUrl}}/picture/${id}/all`),
-  insert: (data) => http.post(`${commonUrl}}/insert`, data),
-  update: (data) => http.post(`${commonUrl}}/update`, data),
+  getAll: () => http.get(`${commonUrl}/all`),
+  getPictures: (id) => http.get(`${commonUrl}/picture/${id}/all`),
+  insert: (data) => http.post(`${commonUrl}/insert`, data),
+  update: (data) => http.post(`${commonUrl}/update`, data),
   searchProjects: (query) => http.get(`${ctx}/project/list/fragment${query}`),
-  starProject: (data) => http.post(`${commonUrl}}/starred`, data),
-  getAllFarm:() => http.get(`${commonUrl}}/farm/all`),
+  starProject: (data) => http.post(`${commonUrl}/starred`, data),
+  getAllFarm:() => http.get(`${commonUrl}/farm/all`),
   selectDividendPoll: (data) =>
     http.post(`${commonUrl}/dividend/poll/select`, data),
 };
